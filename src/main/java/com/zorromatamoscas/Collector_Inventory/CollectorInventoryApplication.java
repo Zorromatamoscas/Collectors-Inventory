8dd6f5ad98e34a2612826ac48e7957216b1483c8
Zorromatamoscas <99506562+Zorromatamoscas@users.noreply.github.com>
2025-10-06 12:20:28 +0200

    proyecto comenzadoç

---
+++ src/main/java/com/zorromatamoscas/Collector_Inventory/CollectorInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.zorromatamoscas.Collector_Inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectorInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectorInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zorromatamoscas/Collector_Inventory/CollectorInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zorromatamoscas.Collector_Inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectorInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
